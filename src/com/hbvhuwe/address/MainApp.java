8d22f9183e940caa787d14ea92fd160c3a82768a
Владимир Черноног <chernonog.vova@gmail.com>
2017-12-26 17:46:43 +0200

    Initial commit

---
+++ src/com/hbvhuwe/address/MainApp.java
@@ -0,0 +1,66 @@
+package com.hbvhuwe.address;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainApp extends Application {
+  private Stage primaryStage;
+  private BorderPane rootLayout;
+
+  @Override
+  public void start(Stage primaryStage) {
+    this.primaryStage = primaryStage;
+    this.primaryStage.setTitle("AddressApp");
+
+    initRootLayout();
+    
+    showPersonOverview();
+  }
+
+  /**
+   * Initialize root component
+   */
+  public void initRootLayout() {
+    try {
+      FXMLLoader loader = new FXMLLoader();
+      loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+      rootLayout = loader.load();
+
+      Scene scene = new Scene(rootLayout);
+      primaryStage.setScene(scene);
+      primaryStage.show();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Show person overview in root component
+   */
+  public void showPersonOverview() {
+    try {
+      FXMLLoader loader = new FXMLLoader();
+      loader.setLocation(MainApp.class.getResource("view/PersonOverview.fxml"));
+      AnchorPane personOverview = loader.load();
+
+      rootLayout.setCenter(personOverview);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public Stage getPrimaryStage() {
+    return primaryStage;
+  }
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+}
