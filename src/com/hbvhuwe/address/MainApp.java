6a7a7ae5b2a6dc8fc89cc4406b2d5c4e29e808ae
Владимир Черноног <chernonog.vova@gmail.com>
2017-12-27 20:25:23 +0200

    add tests & model class
    Add Person model class
    Add TestPersonModel class to test constructor of Person & getters and setters of this class

--- src/com/hbvhuwe/address/MainApp.java
+++ src/com/hbvhuwe/address/MainApp.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
---
+++ src/com/hbvhuwe/address/model/Person.java
@@ -0,0 +1,112 @@
+package com.hbvhuwe.address.model;
+
+import javafx.beans.property.*;
+import org.jetbrains.annotations.Nullable;
+
+import java.time.LocalDate;
+
+/**
+ * Model class for person
+ */
+public class Person {
+  private final StringProperty firstName;
+  private final StringProperty lastName;
+  private final StringProperty street;
+  private final IntegerProperty postalCode;
+  private final StringProperty city;
+  private final ObjectProperty<LocalDate> birthday;
+
+  /**
+   * Default constructor
+   */
+  public Person() {
+    this(null, null);
+  }
+
+  /**
+   * Constructor with some initial data
+   * @param firstName
+   * @param lastName
+   */
+  public Person(@Nullable String firstName, @Nullable String lastName) {
+    this.firstName = new SimpleStringProperty(firstName);
+    this.lastName = new SimpleStringProperty(lastName);
+
+    this.street = new SimpleStringProperty("street");
+    this.postalCode = new SimpleIntegerProperty(1111);
+    this.city = new SimpleStringProperty("city");
+    this.birthday = new SimpleObjectProperty<LocalDate>(LocalDate.of(1970, 1, 1));
+  }
+
+  public String getFirstName() {
+    return firstName.get();
+  }
+
+  public StringProperty firstNameProperty() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName.set(firstName);
+  }
+
+  public String getLastName() {
+    return lastName.get();
+  }
+
+  public StringProperty lastNameProperty() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName.set(lastName);
+  }
+
+  public String getStreet() {
+    return street.get();
+  }
+
+  public StringProperty streetProperty() {
+    return street;
+  }
+
+  public void setStreet(String street) {
+    this.street.set(street);
+  }
+
+  public int getPostalCode() {
+    return postalCode.get();
+  }
+
+  public IntegerProperty postalCodeProperty() {
+    return postalCode;
+  }
+
+  public void setPostalCode(int postalCode) {
+    this.postalCode.set(postalCode);
+  }
+
+  public String getCity() {
+    return city.get();
+  }
+
+  public StringProperty cityProperty() {
+    return city;
+  }
+
+  public void setCity(String city) {
+    this.city.set(city);
+  }
+
+  public LocalDate getBirthday() {
+    return birthday.get();
+  }
+
+  public ObjectProperty<LocalDate> birthdayProperty() {
+    return birthday;
+  }
+
+  public void setBirthday(LocalDate birthday) {
+    this.birthday.set(birthday);
+  }
+}
